d3f7f524e0cd8c2e9e477ab6c0adc42a20713487
Preethika Somarapu <91421615+pree5103@users.noreply.github.com>
2025-02-17 17:24:21 -0600

    Unigram and bigram methods
    Added methods to calculate unigram and bigram probabilities given a specific review and word parameters.

--- main.py
+++ main.py
@@ -31,4 +31,44 @@ for rev in reviews:
     # increment revNum
     revNum += 1
 
+# UNIGRAM 
+def unigram(revNum, word):
+    #holds the data for the specific review we're analyzing
+    review = reviewData[revNum]
+    #the total words in the review
+    totalWords = sum(review.values())
+    #the number of occurences of the word in the review
+    wordCount = review[word]
+    #the probability of the unigram
+    prob = wordCount / totalWords
+    #return the probability rounded to 2 digits
+    return round(prob, 2)
 
+#BIGRAM
+def bigram(revNum, word1, word2):
+    #populate review with the specified review
+    review = reviews[revNum]
+    # translator to get rid of punctuation in the review
+    translator = review.maketrans('', '', string.punctuation)
+    review = review.translate(translator)
+    # split the review to get the words in order
+    review = review.split()
+    #count of the bigrams where word2 follows word1
+    correctPairs = 0
+    #count of the bigrams where word1 is the first word in the word pair
+    wordPairs = 0
+    #loop through all of the words in the review
+    for i in range(len(review) - 1):
+        #if a correct word pair is found, increment correctPairs and wordPairs
+        if review[i] == word1 and review[i + 1] == word2:
+            correctPairs += 1
+            wordPairs += 1
+        #if a word pair where word1 is the first word is found, increment wordPairs
+        elif review[i] == word1 and review[i + 1] != word2:
+            wordPairs += 1
+    #probability is the percentage of correctPairs in all of the found wordPairs
+    probability = correctPairs / wordPairs
+    return probability
+
+#print(unigram(0, "the"))
+#print(bigram(0, "they", "were"))
